8a84c9e3b20c941655b4fd04ca5efca49d9f77a6
BradleyTim <timbradley94@gmail.com>
2020-01-18 17:44:11 +0300

    add landing page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingpageComponent
   ],
   imports: [
     BrowserModule
