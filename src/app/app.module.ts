6896cc4c08a27bb9a8620659500009de74a1723c
BradleyTim <timbradley94@gmail.com>
2020-01-18 18:52:38 +0300

    set up routing module and routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
+import { AboutComponent } from './about/about.component';
 
-
+const appRoutes: Routes = [
+  { path: 'about', component: AboutComponent},
+  { path: '', component: LandingpageComponent},
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LandingpageComponent
+    LandingpageComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
