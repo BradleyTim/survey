fbcb4acae4bf1034775668f50a7b8d9fd5cb5172
BradleyTim <timbradley94@gmail.com>
2020-01-20 10:00:16 +0300

    install and setup firebase dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+// Firebase
+// import { AngularFireModule } from '@angular/fire';
+// import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 
 import { AppComponent } from './app.component';
@@ -8,9 +15,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LandingpageComponent } from './landingpage/landingpage.component';
 import { AboutComponent } from './about/about.component';
 import { SuccessComponent } from './success/success.component';
+import { SurveyformComponent } from './surveyform/surveyform.component';
 
 const appRoutes: Routes = [
   { path: 'success', component: SuccessComponent},
+  { path: 'survey', component: SurveyformComponent},
   { path: 'about', component: AboutComponent},
   { path: '', component: LandingpageComponent},
   { path: '**', redirectTo: '', pathMatch: 'full' }
@@ -21,11 +30,16 @@ const appRoutes: Routes = [
     NavbarComponent,
     LandingpageComponent,
     AboutComponent,
-    SuccessComponent
+    SuccessComponent,
+    SurveyformComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    HttpClientModule,
+    // AngularFireModule.initializeApp(environment.firebaseConfig, 'simbasurvey'),
+    // AngularFirestoreModule, // Only required for database features
   ],
   providers: [],
   bootstrap: [AppComponent]
