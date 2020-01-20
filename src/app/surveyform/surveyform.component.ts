80b22df70a2bfe1b32b9c1bab2a0a77bf6662e34
BradleyTim <timbradley94@gmail.com>
2020-01-20 09:57:45 +0300

    add survey form component and its functionalities

--- src/app/surveyform/surveyform.component.ts
+++ src/app/surveyform/surveyform.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { Survey } from '../shared/survey';
+import { DataService } from '../shared/data.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-surveyform',
   templateUrl: './surveyform.component.html',
-  styleUrls: ['./surveyform.component.css']
+  styleUrls: ['./surveyform.component.css'],
+  providers: [DataService]
 })
 export class SurveyformComponent implements OnInit {
 
@@ -23,9 +27,35 @@ export class SurveyformComponent implements OnInit {
     'Excellent'
   ];
 
-  constructor() { }
+  survey = new Survey(
+    'Name',
+    'exmail@example.com',
+    'How did you get to know us..',
+    'Where did you purchase Simba Governor',
+    'What made you purchase the Simba Speed Governor',
+    'Any issues?',
+    'Your occupation',
+    'Any benefits..',
+    'Age group',
+    'Address or Estate',
+    'Rate us',
+    'Suggestions'
+  );
+
+  surveys: any[];
+
+  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    // this.dataService.getSurveys().subscribe(data => console.log(data));
+    this.surveys = this.dataService.getSurveys();
+    console.log(this.surveys);
+  }
+
+  onSubmit(): void {
+    console.log(this.survey);
+    this.dataService.addSurvey(this.survey);
+    this.router.navigate(["/success"]);
   }
 
 }
