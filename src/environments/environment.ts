61322f6d065be91258a8a8f7fe5567ea405e1fcd
BradleyTim <timbradley94@gmail.com>
2020-01-20 09:55:00 +0300

    add firebase config values in environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyA28pLJD5bLqbk3AzZKf7KabFwqPXFHOwU",
+    authDomain: "survey-app-19358.firebaseapp.com",
+    databaseURL: "https://survey-app-19358.firebaseio.com",
+    projectId: "survey-app-19358",
+    storageBucket: "survey-app-19358.appspot.com",
+    messagingSenderId: "992498168879",
+    appId: "1:992498168879:web:02a731256301704a9782da",
+    measurementId: "G-87F06MQD88"
+  }
 };
